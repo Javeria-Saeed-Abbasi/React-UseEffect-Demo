d1092ef3676c93e2fe34ff3646e8265161c42f5a
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2021-09-15 01:45:52 +0500

    logo
    logo image showed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo2 from './logo2.png';
+import logo2 from '../images/logo2.PNG';
 import App from '../App.css';
 
 
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <div className="header">
             <div className="logo">
-                LOGO {/*<img src={require('./logo2.png')} alt="Logo"/> */}
+                <img src={logo2} alt="Logo" width="100%" height="100"/>
             </div>
             <h1>Food Searching App</h1>
         </div>
